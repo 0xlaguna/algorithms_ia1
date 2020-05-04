bf7e495d46c4e1ec664eb33b30d6e77e2dd916d8
monzork <malonzoareas@gmail.com>
2020-05-03 22:54:35 -0600

    agregado a estrellla

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import sys
 import time
 from graph import *
 from priority_queue import *
-
+from node import *
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtGui as qtg
 from PyQt5 import QtCore as qtc
 from PyQt5.QtCore import Qt
+
 
 
 class MainWindow(qtw.QMainWindow):
@@ -91,8 +92,15 @@ class MainWindow(qtw.QMainWindow):
             else:
                 print('\nUnfulfilled goal.\n')
 
+    def __eq__(self, other):
+        return self.position == other.position
 
     def __init__(self):
+        self.parent = None
+        self.position = None
+        self.g = 0
+        self.h = 0
+        self.f = 0
         super().__init__()
         self.initUi()
 
@@ -129,6 +137,8 @@ class MainWindow(qtw.QMainWindow):
 
         AestrellaAct = qtw.QAction(qtg.QIcon('code-icon.png'), '&A*', self)
 
+        
+        AestrellaAct.triggered.connect(self.astar)
         algoritCat3.addAction(costoUniformAct)
         algoritCat3.addAction(AestrellaAct)
 
@@ -187,6 +197,105 @@ class MainWindow(qtw.QMainWindow):
 
         print(self.bfs(graph, 'A'))
     
+    def astar(self):
+
+        maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+        start = (0, 0)
+        end = (7, 6)
+
+        """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+
+        # Create start and end node
+        start_node = NodeA(None, start)
+        start_node.g = start_node.h = start_node.f = 0
+        end_node = NodeA(None, end)
+        end_node.g = end_node.h = end_node.f = 0
+
+        # Initialize both open and closed list
+        open_list = []
+        closed_list = []
+
+        # Add the start node
+        open_list.append(start_node)
+
+        # Loop until you find the end
+        while len(open_list) > 0:
+
+            # Get the current node
+            current_node = open_list[0]
+            current_index = 0
+            for index, item in enumerate(open_list):
+                if item.f < current_node.f:
+                    current_node = item
+                    current_index = index
+
+            # Pop current off open list, add to closed list
+            open_list.pop(current_index)
+            closed_list.append(current_node)
+
+            # Found the goal
+            if current_node == end_node:
+                path = []
+                current = current_node
+                while current is not None:
+                    path.append(current.position)
+                    current = current.parent
+
+                print(path)
+                return path[::-1] # Return reversed path
+
+            # Generate children
+            children = []
+            for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+
+                # Get node position
+                node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+                # Make sure within range
+                if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                    continue
+
+                # Make sure walkable terrain
+                if maze[node_position[0]][node_position[1]] != 0:
+                    continue
+
+                # Create new node
+                new_node = NodeA(current_node, node_position)
+
+                # Append
+                children.append(new_node)
+
+            # Loop through children
+            for child in children:
+
+                # Child is on the closed list
+                for closed_child in closed_list:
+                    if child == closed_child:
+                        continue
+
+                # Create the f, g, and h values
+                child.g = current_node.g + 1
+                child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+                child.f = child.g + child.h
+
+                # Child is already in the open list
+                for open_node in open_list:
+                    if child == open_node and child.g > open_node.g:
+                        continue
+
+                # Add the child to the open list
+                open_list.append(child)
+
 
 
 
